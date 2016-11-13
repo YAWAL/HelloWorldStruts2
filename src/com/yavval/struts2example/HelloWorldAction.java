99a589b36861bfa95f8fe390db6d8ed3926ad30d
yawal <3dprinter.lviv@gmail.com>
2016-11-13 21:50:08 +0200

    Project prepared for the next configuring

--- src/com/yavval/struts2example/HelloWorldAction.java
+++ src/com/yavval/struts2example/HelloWorldAction.java
@@ -1,11 +1,13 @@
 package com.yavval.struts2example;
 
+import com.opensymphony.xwork2.ActionSupport;
+
 /*
  * The Action class responds to a user action when user clicks a URL. 
  * One or more of the Action class's methods are executed and a String result is returned. 
  * Based on the value of the result, a specific JSP page is rendered.
  */
-public class HelloWorldAction {
+public class HelloWorldAction extends ActionSupport {
 
 	private String name;
 
